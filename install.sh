fbfaaa2f2768e07972a1589be9458968ad352dde
pkd <paulkronlund@gmail.com>
2023-08-19 22:06:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-install -D -v -r opt $DESTDIR/
+install -D -v opt/google/drive/* $DESTDIR/opt/google/drive
 install -D -v mount-drive umount-drive $DESTDIR/bin
 chmod +x $DESTDIR/bin/mount-drive $DESTDIR/bin/umount-drive

ef9fa0d5cd44fb345a8a5292a2a04052242e5974
pkd <paulkronlund@gmail.com>
2023-08-20 16:42:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 install -D -v opt/google/drive/drive $DESTDIR/opt/google/drive/drive
 install -D -v opt/google/drive/libfuse.so $DESTDIR/opt/google/libfuse.so
-install -D -v mount-drive $DESTDIR/bin/mount-drive
-install -D -v umount-drive $DESTDIR/bin/umount-drive
-chmod +x $DESTDIR/bin/mount-drive $DESTDIR/bin/umount-drive
+install -D -v mount-drive $DESTDIR/usr/bin/mount-drive
+install -D -v umount-drive $DESTDIR/usr/bin/umount-drive
+chmod +x $DESTDIR/usr/bin/mount-drive $DESTDIR/usr/bin/umount-drive

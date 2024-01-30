3a2982fb9445601f579cc72c37c0b1146d9ee85f
pkd <paulkronlund@gmail.com>
2024-01-30 10:22:51 +0100

    fixing install error

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 install -D -v opt/google/drive/drive $DESTDIR/opt/google/drive/drive
-install -D -v opt/google/drive/libfuse.so $DESTDIR/opt/google/libfuse.so
+install -D -v opt/google/drive/libfuse.so $DESTDIR/opt/google/drive/libfuse.so
 install -D -v mount-drive $DESTDIR/usr/bin/mount-drive
 install -D -v umount-drive $DESTDIR/usr/bin/umount-drive
 chmod +x $DESTDIR/usr/bin/mount-drive $DESTDIR/usr/bin/umount-drive
